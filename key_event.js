777b0e714d8d5b02444255260cd36144a65bf194
Greg Anderson <gregory.anderson@familysearch.org>
2015-05-15 14:05:47 -0600

    Closes #5 Can scroll to top or bottom wth g and G

--- key_event.js
+++ key_event.js
@@ -2,18 +2,30 @@ if (window == top) {
 	window.addEventListener('keypress', doKeyPress, false); //add the keyboard handler
 }
  
-trigger_key_j = 106; // j key
-trigger_key_k = 107; // k key
+trigger_key_j = 106; // j
+trigger_key_k = 107; // k
+trigger_key_g = 103; // g
+trigger_key_G = 71; // G
 function doKeyPress(e){
 	// Make sure the user isn't in a text box trying to type
 	if (document.activeElement.nodeName == 'TEXTAREA' || document.activeElement.nodeName == 'INPUT') {
 		return;
 	}
+	// Scroll down
 	if (e.keyCode == trigger_key_j){
 		window.scrollBy(0,50);
 	}
+	// Scroll up
 	else if (e.keyCode == trigger_key_k) {
 		window.scrollBy(0,-50);
+	}
+	// Scroll to top
+	else if (e.keyCode == trigger_key_g) {
+		window.scrollTo(0,0);
+	}
+	// Scroll to bottom
+	else if (e.keyCode == trigger_key_G) {
+		window.scrollTo(0,document.body.scrollHeight);
 	}
 	else {
 		alert(e.keyCode);
