ee12abd71886ebc8844c3a48375202464dcc547b
Greg Anderson <gregory.anderson@familysearch.org>
2015-05-14 20:57:35 -0600

    Fixed #3 Now can keep scrolling when holding j or k
    Issue was that the event was 'keyup' instead of 'keypress'.  It also seems that
    changing that needed the letter values to be changed to their ASCII values of
    106 (for j) and 107 (for k).

--- key_event.js
+++ key_event.js
@@ -1,14 +1,17 @@
 if (window == top) {
-	window.addEventListener('keyup', doKeyPress, false); //add the keyboard handler
+	window.addEventListener('keypress', doKeyPress, false); //add the keyboard handler
 }
  
-trigger_key_j = 74; // j key
-trigger_key_k = 75; // k key
+trigger_key_j = 106; // j key
+trigger_key_k = 107; // k key
 function doKeyPress(e){
 	if (e.keyCode == trigger_key_j){
 		window.scrollBy(0,50);
 	}
 	else if (e.keyCode == trigger_key_k) {
 		window.scrollBy(0,-50);
+	}
+	else {
+		alert(e.keyCode);
 	}
 }
